4f6639140544a992d3853ac000a7602c215ddc52
Jonas Vetsch <jonas@vetsch.com>
2024-06-03 14:56:38 +0200

    - add FlightLogsView - add NavigationStack to HomeView - embed gear symbol on HomeView into a NavigationLink with the destination ot FlightLogsView

---
+++ Trainingsbarometer/FlightLogs/FlightLogsView.swift
@@ -0,0 +1,18 @@
+//
+//  FlightLogsView.swift
+//  Trainingsbarometer
+//
+//  Created by Jonas Vetsch on 03.06.2024.
+//
+
+import SwiftUI
+
+struct FlightLogsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlightLogsView()
+}

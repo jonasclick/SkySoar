25c049d33abafbb8f595524111eb6a8dd87ecb11
Jonas Vetsch <jonas@vetsch.com>
2024-06-03 11:56:51 +0200

    Initial Commit

---
+++ Trainingsbarometer/TrainingsbarometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingsbarometerApp.swift
+//  Trainingsbarometer
+//
+//  Created by Jonas Vetsch on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingsbarometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
